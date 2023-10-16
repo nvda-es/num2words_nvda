20bea3596ba8b2fab730043c3652357f7f438dc2
rmcpantoja <angelitomateocedillo@gmail.com>
2023-10-16 15:13:02 -0500

    Updated languages.

---
+++ merge_pot.bat
@@ -0,0 +1,3 @@
+msgmerge -U addon/locale/es/LC_MESSAGES/nvda.po num2words.pot
+msgmerge -U addon/locale/tr/LC_MESSAGES/nvda.po num2words.pot
+msgmerge -U addon/locale/uk/LC_MESSAGES/nvda.po num2words.pot
